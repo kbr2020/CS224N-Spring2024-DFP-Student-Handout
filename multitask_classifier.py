635638bc16e70734dea650f2b5c29b17c7afa9c9
kbr2020 <kbr2020@hotmail.com>
2024-05-20 01:14:22 -0700

    last

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -248,6 +248,7 @@ def train_multitask(args):
             b_mask1 = b_mask1.to(device)
             b_ids2 = b_ids2.to(device)
             b_mask2 = b_mask2.to(device)
+            b_labels = b_labels.to(device)
 
             optimizer.zero_grad()
             logit = model.predict_paraphrase(b_ids1, b_mask1,
@@ -270,7 +271,7 @@ def train_multitask(args):
 
             optimizer.zero_grad()
             logits = model.predict_sentiment(b_ids, b_mask)
-            loss = F.binary_cross_entropy_with_logits(logit.view(-1),b_labels.float(), reduction='sum') / args.batch_size
+            loss = F.cross_entropy(logits, b_labels.view(-1), reduction='sum') / args.batch_size
 
             loss.backward()
             optimizer.step()
@@ -291,6 +292,7 @@ def train_multitask(args):
             b_mask1 = b_mask1.to(device)
             b_ids2 = b_ids2.to(device)
             b_mask2 = b_mask2.to(device)
+            b_labels = b_labels.to(device)
 
             optimizer.zero_grad()
             logit = model.predict_similarity(b_ids1, b_mask1,
