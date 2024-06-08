1966167e4df21d687aafe7ec91cbba05d16a0813
kbr2020 <kbr2020@hotmail.com>
2024-06-07 17:43:07 -0700

    Update multitask_classifier.py

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -412,19 +412,18 @@ def train_multitask(args):
 
         if args.diff_heads:
             best_dev_acc = dev_acc
-            save_model(model, optimizer, args, config, args.filepath)
 
-            if best_dev_accuracies_sst > sts_corr:
+            if best_dev_accuracies_sst < sts_corr:
                 print("BEST accu_sst")
                 best_dev_accuracies_sst = sentiment_accuracy
                 best_model.linear_sst = model.linear_sst
 
-            if best_dev_accuracies_para > paraphrase_accuracy:
+            if best_dev_accuracies_para < paraphrase_accuracy:
                 print("BEST accu_para")
                 best_dev_accuracies_para = paraphrase_accuracy
                 best_model.linear_par = model.linear_par
             
-            if best_dev_accuracies_sts > sts_corr:
+            if best_dev_accuracies_sts < sts_corr:
                 print("BEST accu_sts")
                 best_dev_accuracies_sts = sts_corr
                 best_model.linear_sts = model.linear_sts
