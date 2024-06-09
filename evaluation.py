b835bd56efcfb51218584377042dbf0c291b4980
kbr2020 <kbr2020@hotmail.com>
2024-06-08 18:41:10 -0700

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -41,7 +41,7 @@ def model_eval_fair(fair_dataloader, model, device):
             z_1 = torch.softmax(logits_1,dim = -1).cpu().numpy()
             z_2 = torch.softmax(logits_2,dim = -1).cpu().numpy()
             print(np.max(z_1,axis = 1))
-            print(np.max(z_1,axis = 2))
+            print(np.max(z_1,axis = 1))
             y_hat_1 = logits_1.argmax(dim=-1).flatten().cpu().numpy()
             y_hat_2 = logits_2.argmax(dim=-1).flatten().cpu().numpy()
 
